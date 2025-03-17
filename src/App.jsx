e32692aac72ed5f5352be68ffb86956f8ef27c39
DiegoEMartinezZ <diermaza.23@gmail.com>
2025-03-16 22:42:16 -0500

    Upload the Click&Meet project using React-Vite and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <>
+      <div className="grid h-screen place-items-center bg-gray-900">
+        <div className="p-8 bg-white text-center rounded-lg shadow-lg">
+          <h1 className="text-2xl font-bold text-gray-900">Landing</h1>
+          <h3 className="text-sm"> Click & Meet</h3>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
