ddb8cfebfe7380152131940b616e121cb4d637db
Melissa Diaz <120139230+melodiaz23@users.noreply.github.com>
2025-04-09 20:57:06 -0500

    add LoginForm and LoginPage components for user authentication

---
+++ src/pages/Login.jsx
@@ -0,0 +1,16 @@
+import Menu from "../components/menu/MenuAdmin";
+import LoginForm from "../UI/forms/login/LoginForm";
+import Navbar from "../UI/navbar/Navbar";
+import Title from "../UI/txt/Title";
+
+const LoginPage = () => {
+  return (
+    <>
+      <Navbar />
+      <Title />
+      <LoginForm />
+    </>
+  );
+};
+
+export default LoginPage;
